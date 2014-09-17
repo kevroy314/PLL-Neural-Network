a351879b94be9c818e3cd62740a6bd6b7465cb76
kevroy314 <kevin.horecka@gmail.com>
2014-09-17 14:03:49 -0500

    Changed PLL to use scipy butterworth lowpass filter. Complex numbers should work by default, but I'm not entirely sure how to impose/resolve the phase offsets yet.

--- PLL_Visualization.py
+++ PLL_Visualization.py
@@ -1,34 +1,32 @@
-from pyqtgraph.Qt import QtGui, QtCore # For GUI
-import numpy as np # For matrix computation
-import pyqtgraph as pg # for GUI
-from biquad_module import Biquad # For filters
-from pylab import * # For PLL
-import random, re # For noise
-from scipy import signal
 from collections import deque
+
+from pyqtgraph.Qt import QtGui, QtCore  # For GUI
+import pyqtgraph as pg  # For GUI
+from pylab import *  # For PLL
+from scipy import signal
+import random
+
 
 # Set up application window
 app = QtGui.QApplication([])
 win = pg.GraphicsWindow(title="PLL Example Animated")
-win.resize(1000,600)
+win.resize(1000, 600)
 win.setWindowTitle('PLL Example Animated')
 pg.setConfigOptions(antialias=True)
 
 # Set up plot environment
 plotArea = win.addPlot()
-plotArea.enableAutoRange('xy',True)
-curves = []
-curves.append(plotArea.plot(pen=(255,0,0)))
-curves.append(plotArea.plot(pen=(0,255,0)))
-curves.append(plotArea.plot(pen=(0,0,255)))
-curves.append(plotArea.plot(pen=(255,255,255)))
+plotArea.enableAutoRange('xy', True)
+curves = [plotArea.plot(pen=(255, 0, 0)), plotArea.plot(pen=(0, 255, 0)), plotArea.plot(pen=(0, 0, 255)),
+          plotArea.plot(pen=(255, 255, 255))]
 
-legend = pg.LegendItem(offset=(0,1))
-legend.addItem(curves[0], "Phase Detected, Lowpass Filtered")
-legend.addItem(curves[1], "PLL Lock")
-legend.addItem(curves[2], "Logical Lock")
+legend = pg.LegendItem(offset=(0, 1))
+legend.addItem(curves[0], "Output")
+legend.addItem(curves[1], "Current Phase Shift")
+legend.addItem(curves[2], "Phase Control")
 legend.addItem(curves[3], "Test Signal")
 legend.setParentItem(plotArea)
+
 
 # Class representing the functioning of a PLL
 # Sample Rate is in Hz
@@ -37,113 +35,122 @@ legend.setParentItem(plotArea)
 # Loop Gain is a proportion (usually 0<x<1)
 # Phase Shift is a proportion (usually 0<=x<1)
 class PLL:
-    def __init__(self, sample_rate, carrier_frequency, loop_gain, phase_shift):
-        self.invsqr2 = 1.0 / sqrt(2.0)
+    def __init__(self, _sample_rate, _carrier_frequency, _voltage_gain, _phase_shift):
+        self.sample_rate = _sample_rate
 
-        self.sample_rate = sample_rate
+        self.current_phase_shift = 0
+        self.next_phase_shift = 0
+        self.persistent_phase_shift = _phase_shift
 
-        self.phase_shift = phase_shift
+        self.carrier_frequency = _carrier_frequency
+        self.voltage_gain = _voltage_gain
 
-        self.pll_integral = 0
-        self.old_ref = 0
-        self.pll_cf = carrier_frequency
-        self.pll_loop_gain = loop_gain
-        self.ref_sig = 0
+        self.voltage_output = 0
 
-        self.pll_loop_control = 0
-        self.output = 0
-        self.pll_lock = 0
-        self.logic_lock = 0
-
-        #self.loop_lowpass = Biquad(Biquad.LOWPASS,100,self.sample_rate,self.invsqr2)
-        #self.output_lowpass = Biquad(Biquad.LOWPASS,10,self.sample_rate,self.invsqr2)
-        #self.lock_lowpass = Biquad(Biquad.LOWPASS,10,self.sample_rate,self.invsqr2)
-
-        self.loop_lowpass_data = deque()
-        self.output_lowpass_data = deque()
-        self.lock_lowpass_data = deque()
+        self.voltage_output_lowpass_data = deque()
         self.window_size = 1000
+        self.lowpass_cutoff_frequency = 10
         self.filter_order = 2
 
-        self.da = []
-        self.db = []
-        self.dc = []
+        self.output_log = []
+        self.current_phase_shift_log = []
+        self.control_log = []
 
-    def butter_lowpass(self, lowcut, fs, order=5):
+    @staticmethod
+    def butter_lowpass(frequency_cutoff, fs, order=5):
         nyq = 0.5 * fs
-        low = lowcut / nyq
+        low = frequency_cutoff / nyq
         b, a = signal.butter(order, low, btype='low')
         return b, a
 
-    def butter_lowpass_filter(self, data, lowcut, fs, order=5):
-        b, a = self.butter_lowpass(lowcut, fs, order=order)
-        y = signal.lfilter(b, a, data)
-        return y
+    def butter_lowpass_filter(self, data, frequency_cutoff, fs, order=5):
+        b, a = self.butter_lowpass(frequency_cutoff, fs, order=order)
+        return signal.lfilter(b, a, data)
 
-    def update(self, t, y):
+    @staticmethod
+    def v(theta):
+        return sin(theta)
+
+    def update(self, _t, _y, _PLLs, _self_index, _weight_matrix, _connectivity_matrix):
         # BEGIN PLL block
-        self.pll_loop_control = y * self.ref_sig * self.pll_loop_gain # phase detector
 
-        self.loop_lowpass_data.append(self.pll_loop_control)
-        if(len(self.loop_lowpass_data) > self.window_size):
-            self.loop_lowpass_data.popleft()
-        self.pll_loop_control = self.butter_lowpass_filter(self.loop_lowpass_data, 100, self.sample_rate, order=self.filter_order)[-1] # loop low-pass filter
-        self.output_lowpass_data.append(self.pll_loop_control)
-        if(len(self.output_lowpass_data) > self.window_size):
-            self.output_lowpass_data.popleft()
-        self.output = self.butter_lowpass_filter(self.output_lowpass_data, 10, self.sample_rate, order=self.filter_order)[-1] # output low-pass filter
+        # Phase Detector
+        control = 0
+        if len(self.output_log) != 0:
+            control = (_y * self.output_log[-1] * self.voltage_gain)
+        self.control_log.append(control)
 
-        self.pll_integral += self.pll_loop_control / self.sample_rate # FM integral
-        self.ref_sig = cos(2 * pi * self.pll_cf * (t + self.pll_integral) + (self.phase_shift * 2 * pi)) # reference signal
-        self.quad_ref = (self.ref_sig - self.old_ref) * self.sample_rate / (2 * pi * self.pll_cf) # quadrature reference
-        self.old_ref = self.ref_sig
+        # Lowpass Filter
+        #integral = self.butter_lowpass_filter(self.control_log, self.lowpass_cutoff_frequency,
+        #                                      self.carrier_frequency, order=self.filter_order)
 
-        self.lock_lowpass_data.append(-self.quad_ref * y)
-        if(len(self.lock_lowpass_data) > self.window_size):
-            self.lock_lowpass_data.popleft()
-        self.pll_lock = self.butter_lowpass_filter(self.lock_lowpass_data, 10, self.sample_rate, order=self.filter_order)[-1] # lock sensor
-        self.logic_lock = (0,1)[self.pll_lock > 0.1] # logical lock
+        # Determine Weighted Phase Adjustment
+        phase_aggregator = 0
+        for _j in range(0, len(PLLs)):
+            v_ij = _connectivity_matrix[_self_index][_j] * cos(_weight_matrix[_self_index][_j]).real
+            w_ij = _connectivity_matrix[_self_index][_j] * sin(_weight_matrix[_self_index][_j]).imag
+
+            phase_aggregator += v_ij * self.v(_PLLs[_j].current_phase_shift - (pi / 2)) + \
+                                w_ij * self.v(_PLLs[_j].current_phase_shift)
+
+        self.next_phase_shift = self.persistent_phase_shift + self.v(control) * phase_aggregator
+
+        # Voltage Controlled Oscillator
+        output = self.v(self.carrier_frequency * _t + self.next_phase_shift)
+
         # END PLL block
-        self.da.append(self.output)
-        self.db.append(self.pll_lock)
-        self.dc.append(self.logic_lock)
+        self.output_log.append(output)
+        self.current_phase_shift_log.append(self.next_phase_shift)
+
+        return output
+
+    def apply_next_phase_shift(self):
+        self.current_phase_shift = self.next_phase_shift
+
 
 # Class representing a test signal to be fed to the PLL
 class SweepSignal:
-    def __init__(self, sample_rate, carrier_frequency, deviation, noise_level, duration):
-        self.fa = []
-        self.modi = 0
-        self.duration = duration
-        self.carrier_frequency = carrier_frequency
-        self.deviation = deviation
-        self.sample_rate = sample_rate
-        self.noise_level = noise_level
+    def __init__(self, _sample_rate, _carrier_frequency, _deviation, _noise_level, _duration):
+        self.signal_log = []
+        self.modulus_i = 0
+        self.duration = _duration
+        self.carrier_frequency = _carrier_frequency
+        self.deviation = _deviation
+        self.sample_rate = _sample_rate
+        self.noise_level = _noise_level
 
-    def ntrp(self,x,xa,xb,ya,yb):
-        return (x-xa) * (yb-ya) / (xb-xa) + ya
+    @staticmethod
+    def sweep_function_core(x, xa, xb, ya, yb):
+        return (x - xa) * (yb - ya) / (xb - xa) + ya
 
-    def update(self, t):
-        sweep_freq = self.ntrp(t, 0, self.duration, self.carrier_frequency - self.deviation, self.carrier_frequency + self.deviation)
-        self.fa.append(sweep_freq)
-        self.modi += (sweep_freq - self.carrier_frequency) / (self.carrier_frequency * self.sample_rate)
-        test_sig = cos(2 * pi * self.carrier_frequency * (t + self.modi))
+    def update(self, _t):
+        sweep_freq = self.sweep_function_core(_t, 0, self.duration,
+                                              self.carrier_frequency - self.deviation,
+                                              self.carrier_frequency + self.deviation)
+        self.signal_log.append(sweep_freq)
+        self.modulus_i += (sweep_freq - self.carrier_frequency) / (self.carrier_frequency * self.sample_rate)
+        test_sig = cos(2 * pi * self.carrier_frequency * (_t + self.modulus_i))
         noise = random.random() * 2 - 1
-        test_sig += noise * self.noise_level 
+        test_sig += noise * self.noise_level
         return test_sig
 
-class SineSignal:
-    def __init__(self, amplitude, frequency, phase, noise_level):
-        self.frequency = frequency
-        self.two_pi_frequency = 2 * pi * frequency
-        self.amplitude = amplitude
-        self.phase = phase * 2 * pi
-        self.noise_level = noise_level
-        self.fa = []
-        self.fa_scaling = 0.001
 
-    def update(self, t):
-        test_sig = self.amplitude * sin(t * self.two_pi_frequency + self.phase) + ((random.random() * 2 - 1) * self.noise_level)
-        self.fa.append(test_sig * self.fa_scaling)
+class SineSignal:
+    def __init__(self, _amplitude, _frequency, _phase, _noise_level):
+        self.frequency = _frequency
+        self.two_pi_frequency = 2 * pi * _frequency
+        self.amplitude = _amplitude
+        self.phase = _phase * 2 * pi
+        self.noise_level = _noise_level
+        self.signal_log = []
+        self.signal_log_scaling = 1
+
+    def update(self, _t):
+        test_sig = \
+            self.amplitude * \
+            sin(_t * self.two_pi_frequency + self.phase) + \
+            ((random.random() * 2 - 1) * self.noise_level)
+        self.signal_log.append(test_sig * self.signal_log_scaling)
         return test_sig
 
 # Simulation Properties
@@ -153,27 +160,43 @@ deviation = 140
 
 # PLL Properties
 loop_gain = 0.05
-phase_shift = 0.0
 number_of_PLLs = 1
+
+phase_weight_matrix = [
+    [1, 0, 0.4],
+    [0, 1, 0.2],
+    [0.4, 0.2, 1]
+]
+
+connectivity_matrix = [
+    [1, 1, 1],
+    [1, 1, 1],
+    [1, 1, 1]
+]
+
+if (np.array(phase_weight_matrix).transpose() != np.array(phase_weight_matrix)).any():
+    print "Error: Phase Offset Matrix Not Symmetric Across Diagonal."
+else:
+    print "Array Symmetry Validated."
 PLLs = []
 
 # Create PLL
 for i in range(0, number_of_PLLs):
-    PLLs.append(PLL(sample_rate, carrier_frequency, loop_gain, phase_shift))
+    PLLs.append(PLL(sample_rate, carrier_frequency, loop_gain, -pi / 2))
 
 # Test Signal Properties
-noise_level = 1
+noise_level = 0.01
 duration = 4
 test_signals = []
 
 # Test input signal variables
 for i in range(0, number_of_PLLs):
-    test_signals.append(SineSignal(1, 10, 0, noise_level))
-    #test_signals.append(SweepSignal(sample_rate, carrier_frequency, deviation, noise_level, duration))
+    test_signals.append(SineSignal(1, 1.2, 0, noise_level))
+    # test_signals.append(SweepSignal(sample_rate, carrier_frequency, deviation, noise_level, duration))
 
 # Time counter
 t = 0
-tick = 1/sample_rate
+tick = 1 / sample_rate
 
 # Decimation for the display to speed up computation
 display_decimation = 100
@@ -183,35 +206,42 @@ display_pll_num = 0
 # Create loop timer
 timer = QtCore.QTimer()
 
-def update():
-    global timer, curves, plotArea, t, tick, frame_counter, duration, PLLs, test_signals
 
-	# Stop the simulation when the duration has completed
+def update():
+    global timer, curves, plotArea, t, tick, frame_counter, duration, \
+        PLLs, test_signals, phase_weight_matrix, connectivity_matrix
+
+    # Stop the simulation when the duration has completed
     if t >= duration:
         timer.stop()
 
     # Update the test signal and the ppl (iterate simulation)
-    for i in range(0,number_of_PLLs):
-        PLLs[i].update(t, test_signals[i].update(t))
+    for _i in range(0, number_of_PLLs):
+        PLLs[_i].update(t, test_signals[_i].update(t), PLLs, _i, phase_weight_matrix, connectivity_matrix)
+    # Update internal states for next iteration
+    # (done separately from previous call to maintain consistent internal state across all iterations)
+    for _i in range(0, number_of_PLLs):
+        PLLs[_i].apply_next_phase_shift()
 
     # Graph the PLL states according to the display decimation
-    if frame_counter%display_decimation == 0:
-        curves[0].setData(PLLs[display_pll_num].da)
-        curves[1].setData(PLLs[display_pll_num].db)
-        curves[2].setData(PLLs[display_pll_num].dc)
-        curves[3].setData(test_signals[display_pll_num].fa)
+    if frame_counter % display_decimation == 0:
+        curves[0].setData(PLLs[display_pll_num].output_log)
+        curves[1].setData(PLLs[display_pll_num].current_phase_shift_log)
+        curves[2].setData(PLLs[display_pll_num].control_log)
+        curves[3].setData(test_signals[display_pll_num].signal_log)
 
     # Iterate the time counter according to the sample rate
     t += tick
-    #Iterate the display frame counter
+    # Iterate the display frame counter
     frame_counter += 1
 
 # Begin Simulation
 timer.timeout.connect(update)
 timer.start(0)
 
-## Start Qt event loop unless running in interactive mode or using pyside.
+# # Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
     import sys
+
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
         QtGui.QApplication.instance().exec_()
